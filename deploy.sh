3e17d973316e4f61ed2399c5b5f70f738f7748df
Daniel G. Taylor <danielgtaylor@gmail.com>
2014-10-06 21:36:51 -0700

    Disable strict host key checking for deployments, should re-enable in the future with the proper key hard-coded

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -avz -e 'ssh -i id_rsa' _site/ kari@gaaflora.com:/var/www/beta.gaaflora.com/
+rsync -avz -e 'ssh -i id_rsa -o StrictHostKeyChecking=no' _site/ kari@gaaflora.com:/var/www/beta.gaaflora.com/
