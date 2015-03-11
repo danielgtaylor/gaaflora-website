a9ffe02fea4b6a74cd7e148b01038069567c9c7b
Daniel G. Taylor <danielgtaylor@gmail.com>
2015-03-10 18:26:06 -0700

    Add a back-to-top button to gallery pages.

--- themes/gaaflora/source/js/gaaflora.js
+++ themes/gaaflora/source/js/gaaflora.js
@@ -1,5 +1,7 @@
 var container = document.querySelector('#gallery-photos');
 var msnry;
+var scrollToTop = document.querySelector('#top-button');
+var scrollToTopShowing = false;
 
 if (container !== null) {
   imagesLoaded(container, function () {
@@ -10,3 +12,16 @@ if (container !== null) {
     });
   });
 }
+
+window.addEventListener('scroll', function () {
+  var name;
+
+  if (!scrollToTopShowing && window.scrollY > 500) {
+    scrollToTop.className += ' visible';
+    scrollToTopShowing = true;
+  } else if (scrollToTopShowing && window.scrollY < 500) {
+    name = scrollToTop.className;
+    scrollToTop.className = name.substr(0, name.length - 8);
+    scrollToTopShowing = false;
+  }
+});
